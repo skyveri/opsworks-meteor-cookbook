ae184c04f3367a6fc37a4c99e31c7569c77298e6
Ildar Samit <ildar@samitwebsites.com>
2014-03-27 19:17:15 -0400

    Update install_meteor.rb

--- opsworks_meteor/recipes/install_meteor.rb
+++ opsworks_meteor/recipes/install_meteor.rb
@@ -1,7 +1,12 @@
 bash "Install Meteor" do
   user "root"
+
+  Chef::Log.debug("Starting Meteor install script.")
+
   code <<-EOF
   curl https://install.meteor.com | /bin/sh
   npm install -g meteorite
   EOF
+
+  Chef::Log.debug("Finished Meteor install script.")
 end
