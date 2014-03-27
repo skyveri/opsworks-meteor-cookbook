44c252943fbb468855f8d2819e616db9181f0df4
Ildar Samit <ildar@samitwebsites.com>
2014-03-26 22:40:52 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -1,5 +1,11 @@
 bash "Install Meteor" do
+  user "root"
+  code <<-EOH
+  echo "Hello OpsWorks World" > /etc/motd
+  EOH
   code <<-EOF
   curl https://install.meteor.com | /bin/sh
-EOF
+  cd /tmp
+  meteor create myapp
+  EOF
 end
