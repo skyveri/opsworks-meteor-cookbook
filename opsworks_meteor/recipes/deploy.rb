e17d1c633b23092503e76c782372dad1ec3ee1ba
Ildar Samit <ildar@samitwebsites.com>
2014-03-26 22:51:43 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -2,6 +2,8 @@ bash "Deploy Meteor" do
   user "root"
   code <<-EOF
   cd /srv/www/skyveri_main_site/current/
+  npm install -g meteorite
+  mrt install
   meteor bundle myapp.tgz
   tar -xzf myapp.tgz
   EOF
