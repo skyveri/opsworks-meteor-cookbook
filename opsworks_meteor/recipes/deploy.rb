8e350ab7a6ffc75829feef0769b54cab4eb9f561
Ildar Samit <ildar@samitwebsites.com>
2014-03-26 22:47:36 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -1,11 +1,8 @@
 bash "Install Meteor" do
   user "root"
-  code <<-EOH
-  echo "Hello OpsWorks World" > /etc/motd
-  EOH
   code <<-EOF
-  curl https://install.meteor.com | /bin/sh
-  cd /tmp
-  meteor create myapp
+  cd /srv/www/skyveri_main_site/current/
+  meteor bundle myapp.tgz
+  tar -xzf myapp.tgz
   EOF
 end
