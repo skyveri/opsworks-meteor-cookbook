1ea1a5425a060023016ccfc751d26e291a1f54ca
Ildar Samit <ildar@samitwebsites.com>
2014-03-27 12:02:15 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -8,7 +8,6 @@ bash "Deploy Meteor" do
   meteor bundle tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
   tar -xzf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
   rm tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
-
   echo "process.env.MONGO_URL = 'mongodb://skyveri_readonly:Feiun5s09@oceanic.mongohq.com:10016/skyveri_main'; process.env.PORT = 80; require('./bundle/main.js'); " > server.js
   chown deploy:www-data /srv/www/skyveri_main_site/current/server.js
   chown -R deploy:www-data /srv/www/skyveri_main_site/current/bundle
