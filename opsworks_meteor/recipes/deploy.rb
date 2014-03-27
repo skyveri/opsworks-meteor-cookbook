7263c0d85667885cfe8140101672b967732e1a6d
Ildar Samit <ildar.samit@gmail.com>
2014-03-26 21:42:14 -0400

    Test deployment

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -1,5 +1,11 @@
 bash "Install Meteor" do
+  user "ubuntu"
+  command "echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'"
+  command "echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'"
+  command "echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'"
+  command "echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'"
+  command "echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'"
+  command "echo 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'"
   command "curl https://install.meteor.com | /bin/sh"
-  # cwd "/tmp"
-  command "pwd"
+  cwd "/srv/www/skyveri_main_site/current"
 end
