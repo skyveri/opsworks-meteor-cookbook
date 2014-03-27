9026ca45906a975440f825f591a41b19aac3df6d
Ildar Samit <ildar@samitwebsites.com>
2014-03-27 10:55:44 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -10,6 +10,7 @@ bash "Deploy Meteor" do
   mrt install
   meteor bundle tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
   tar -xzf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
+  rm -rf /srv/www/skyveri_main_site/current/bundle
   mv bundle /srv/www/skyveri_main_site/current/
   cd /srv/www/skyveri_main_site/current/
   echo "process.env.MONGO_URL = 'mongodb://skyveri_readonly:Feiun5s09@oceanic.mongohq.com:10016/skyveri_main'; process.env.PORT = 80; require('./bundle/main.js'); " > server.js
