8bb2e8d3e3c91b8234df4ef8a9fc5e240a55275d
Ildar Samit <ildar@samitwebsites.com>
2014-03-27 12:01:39 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -1,29 +1,17 @@
-bash "Get repo" do
-  user "ubuntu"
-  code <<-EOF
-  cd /tmp/
-  rm -rf meteor_tmp
-  mkdir -p meteor_tmp
-  cd /tmp/meteor_tmp/
-  ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-  git clone git@github.com:skyveri/skyveri-main-site.git ./repo
-  cd /tmp/meteor_tmp/repo/
-  mrt install
-  meteor bundle tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
-  tar -xzf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
-  EOF
-end
-
 bash "Deploy Meteor" do
   user "root"
   code <<-EOF
-  cd /tmp/meteor_tmp/repo/
-  rm -rf /srv/www/skyveri_main_site/current/bundle
-  mkdir -p /srv/www/skyveri_main_site/current/
-  mv bundle /srv/www/skyveri_main_site/current/
-  chown -R deploy:www-data /srv/www/skyveri_main_site/current/
   cd /srv/www/skyveri_main_site/current/
+  rm -rf ./bundle
+  rm -rf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
+  mrt install
+  meteor bundle tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
+  tar -xzf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
+  rm tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
+
   echo "process.env.MONGO_URL = 'mongodb://skyveri_readonly:Feiun5s09@oceanic.mongohq.com:10016/skyveri_main'; process.env.PORT = 80; require('./bundle/main.js'); " > server.js
+  chown deploy:www-data /srv/www/skyveri_main_site/current/server.js
+  chown -R deploy:www-data /srv/www/skyveri_main_site/current/bundle
   monit restart node_web_app_skyveri_main_site
   EOF
 end
