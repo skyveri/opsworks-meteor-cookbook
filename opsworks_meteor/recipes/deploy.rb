b9af0f5a722e0223ab510d12f279527583018b7d
Ildar Samit <ildar@samitwebsites.com>
2014-03-27 11:23:55 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -11,7 +11,9 @@ bash "Deploy Meteor" do
   meteor bundle tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
   tar -xzf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
   rm -rf /srv/www/skyveri_main_site/current/bundle
+  mkdir -p /srv/www/skyveri_main_site/current/
   mv bundle /srv/www/skyveri_main_site/current/
+  chown -R deploy:www-data /srv/www/skyveri_main_site/current/
   cd /srv/www/skyveri_main_site/current/
   echo "process.env.MONGO_URL = 'mongodb://skyveri_readonly:Feiun5s09@oceanic.mongohq.com:10016/skyveri_main'; process.env.PORT = 80; require('./bundle/main.js'); " > server.js
   monit restart node_web_app_skyveri_main_site
