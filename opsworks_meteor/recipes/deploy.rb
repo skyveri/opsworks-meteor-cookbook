05b5702e3c2e22cb96b10010784805ee2c1e8be4
Ildar Samit <ildar@samitwebsites.com>
2014-03-28 17:29:01 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -26,12 +26,24 @@ node[:deploy].each do |app_slug_name, app_deploy|
         # Reset the Meteor temp directory
         command "rm -rf #{tmp_dir}"
         command "mkdir -p #{tmp_dir}"
-        # Move files to the temp directory
-        command "cp #{repo_dir}/. #{tmp_dir} -R"
+        # # Move files to the temp directory
+        # command "cp #{repo_dir}/. #{tmp_dir} -R"
 
-        # Create a Meteor bundle
-        cwd "#{tmp_dir}"
-        command "mrt install"
+        # # Create a Meteor bundle
+        # cwd "#{tmp_dir}"
+        # command "mrt install"
+        # command "meteor bundle bundled_app.tgz"
+        # command "tar -xzf bundled_app.tgz"
+
+        # # Copy the bundle folder into the release directory
+        # command "cp #{tmp_dir}/bundle #{current_release} -R"
+        # command "chown -R deploy:www-data #{current_release}/bundle"
+
+        # # OpsWorks expects a server.js file
+        # command "echo \"process.env.ROOT_URL  = '#{protocol_prefix}#{domain_name}';\" > #{current_release}/server.js"
+        # command "echo \"process.env.MONGO_URL = '#{mongo_url}';\" >> #{current_release}/server.js"
+        # command "echo \"process.env.PORT = 80; require('./bundle/main.js');\" >> #{current_release}/server.js"
+        # command "chown deploy:www-data #{current_release}/server.js"
       end
     end
   end
