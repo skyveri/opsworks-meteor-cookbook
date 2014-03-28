d650c6dac585371dda1121b5d95b82a69e22d96c
Ildar Samit <ildar@samitwebsites.com>
2014-03-28 12:17:50 -0400

    Update deploy.rb

--- opsworks_meteor/recipes/deploy.rb
+++ opsworks_meteor/recipes/deploy.rb
@@ -1,46 +1,46 @@
 node[:deploy].each do |app_slug_name, deploy|
   deploy deploy[:deploy_to] do
-  
-    before_symlink do
-      Chef::Log.debug("before_symlink script.")
-      
+
+    before_restart do
+      Chef::Log.debug("before_restart script.")
+
       bash "Deploy Meteor" do
         user "root"
-      
+
         Chef::Log.debug("Starting to loop through each app.")
-      
+
         node[:deploy].each do |app_slug_name, deploy|
-      
+
           if deploy[:domains].length == 0
             Chef::Log.debug("Skipping deploy::nodejs application #{application} does not have any domains configured.")
             next
           end
-      
+
           domain_name = deploy[:domains][0]
-      
+
           if deploy[:ssl_support]
             protocol_prefix = "https://"
           else
             protocol_prefix = "http://"
           end
-        
+
           Chef::Log.debug("Using the first domain to create ROOT_URL for Meteor.")
           Chef::Log.debug("ROOT_URL: #{protocol_prefix}#{domain_name}")
           Chef::Log.debug("App slug name (the app_slug_name variable): #{app_slug_name}")
-        
+
           code <<-EOF
-      
+
           cd /srv/www/#{app_slug_name}/current/
-      
+
           rm -rf /tmp/meteor_tmp
           mkdir -p /tmp/meteor_tmp
-      
+
           mv ./config /tmp/meteor_tmp
           mv ./log /tmp/meteor_tmp
           mv ./opsworks.js /tmp/meteor_tmp
           mv ./public /tmp/meteor_tmp
           mv ./tmp /tmp/meteor_tmp
-      
+
           mrt install
           rm -rf tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
           meteor bundle tmp_f90e9fkjkjf0s0esre0r9034932952359sfd90.tgz
@@ -52,26 +52,24 @@ node[:deploy].each do |app_slug_name, deploy|
           echo "process.env.PORT = 80; require('./bundle/main.js'); " >> server.js
           chown    deploy:www-data ./server.js
           chown -R deploy:www-data ./bundle
-      
+
           mv /tmp/meteor_tmp/config ./
           mv /tmp/meteor_tmp/log ./
           mv /tmp/meteor_tmp/opsworks.js ./
           mv /tmp/meteor_tmp/public ./
           mv /tmp/meteor_tmp/tmp ./
           EOF
-      
-          sleep 10
-      
+
           Chef::Log.debug("Restarting node.")
-      
+
           code <<-EOF
           monit restart node_web_app_#{app_slug_name}
           EOF
-      
+
           Chef::Log.debug("Finished running commands for app #{app_slug_name}.")
-      
+
         end
-      
+
       end
 
 
